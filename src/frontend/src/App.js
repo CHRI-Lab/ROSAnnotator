517e250dccbee1bba175062e10998923b3726358
Bowen Fan <bffa@student.unimelb.edu.au>
2024-03-29 18:34:51 +1100

    Added navigation bar

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import AppRoutes from './routes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+import Home from './pages/Home';
+import About from './pages/About';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+
+  const pages = [
+    {name: 'Home', path: '/', page: <Home />},
+    {name: 'About', path: '/about', page: <About />}
+  ]
+
   return (
-    <AppRoutes />
+    <BrowserRouter>
+      <NavigationBar pages={pages}/>
+      <Routes>
+        {pages.map((p) => {
+          return <Route path={p.path} page={p.page} />
+        })}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
