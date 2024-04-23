df49ae4e4c51612af4933ef2527b38b0d3fffcc3
UMzheng <yujiezheng@student.unimelb.edu.au>
2024-04-23 19:13:59 +1000

    fix minor bug in previous api and add waveform function

--- src/backend/rosbag_processing/views.py
+++ src/backend/rosbag_processing/views.py
@@ -1,16 +1,17 @@
+import base64
 import os
 import subprocess
-from rosbag.bag import Bag
 import cv2
+import yaml
+import numpy as np
+from rosbag.bag import Bag
 from cv_bridge import CvBridge
 from google.cloud import speech_v1p1beta1 as speech
 from datetime import datetime
-import uuid
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-def generate_unique_id():
-    return str(uuid.uuid4())
+import matplotlib.pyplot as plt
+from pydub import AudioSegment
 
 def extract_images_from_rosbag(bag_filename, output_folder):
     bag = Bag(bag_filename, 'r')
@@ -20,16 +21,23 @@ def extract_images_from_rosbag(bag_filename, output_folder):
         cv2_image = bridge.imgmsg_to_cv2(message, desired_encoding='passthrough')
         cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
         cv2.imwrite(f'{output_folder}/images/frame{i:04d}.jpg', cv2_image)
-
-def extract_video(output_folder):
+        
+def extract_video(bag_filename, output_folder):
 
     def get_fps():
+        bag = Bag(bag_filename, 'r')
+        bag_info = yaml.load(bag._get_yaml_info(), Loader=yaml.Loader)
         for topic in bag_info['topics']:
             if topic['type'] == 'sensor_msgs/Image':
                 return topic['frequency']
         return None
-    subprocess.run(['ffmpeg', '-r', str(get_fps()), '-i', r'images/frame%04d.jpg', 'video.avi'])
-    # subprocess.run(['ffmpeg', '-i', f'{output_folder}/video.avi', '-i', f'{output_folder}/audio.mp3', '-c:v', 'copy', '-map', '0:v', '-map', '1:a', '-y', f'{output_folder}/output.mp4'])
+    
+    fps = str(get_fps())
+
+    input_images_path = os.path.join(output_folder, 'images/frame%04d.jpg')
+    output_video_path = os.path.join(output_folder, 'video.avi')
+    subprocess.run(['ffmpeg', '-r', str(fps), '-i', input_images_path, output_video_path])
+
 
 def extract_audio(bag_filename, output_folder):
     bag = Bag(bag_filename, 'r')
@@ -39,10 +47,28 @@ def extract_audio(bag_filename, output_folder):
                 # f.write(int.to_bytes(byte))
                 f.write(byte.to_bytes(1, 'big'))
 
+def plot_waveform(audio_path, output_filename, start_sec=None, end_sec=None):
+    audio = AudioSegment.from_file(audio_path)
+    if start_sec is not None and end_sec is not None:
+        start_ms = start_sec * 1000 
+        end_ms = end_sec * 1000
+        audio = audio[start_ms:end_ms]
+    if audio.channels > 1:
+        audio = audio.set_channels(1)
+    samples = np.array(audio.get_array_of_samples())
+    plt.figure(figsize=(14, 5))
+    plt.plot(samples, color='black', linewidth=0.5)
+    plt.axis('off')
+    plt.savefig(output_filename, bbox_inches='tight', pad_inches=0)
+    plt.close()
 
 def combine_video_audio(output_folder):
-    # subprocess.run(['ffmpeg', '-i', 'video.avi', '-i', 'audio.mp3', '-c:v', 'copy', '-map', '0:v', '-map', '1:a', '-y', 'output.mp4'])
-    subprocess.run(['ffmpeg', '-i', f'{output_folder}/video.avi', '-i', f'{output_folder}/audio.mp3', '-c:v', 'copy', '-map', '0:v', '-map', '1:a', '-y', f'{output_folder}/output.mp4'])
+
+    video_path = os.path.join(output_folder, 'video.avi')
+    audio_path = os.path.join(output_folder, 'audio.mp3')
+    output_path = os.path.join(output_folder, 'output.mp4')
+
+    subprocess.run(['ffmpeg', '-i', video_path, '-i', audio_path, '-c:v', 'copy', '-map', '0:v', '-map', '1:a', '-y', output_path])
 
 def generate_srt(uri, output_folder):
     # Create a Speech client
@@ -97,6 +123,11 @@ def format_time(duration):
 
     return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
 
+def encode_file_base64(file_path):
+    with open(file_path, 'rb') as file:
+        encoded_string = base64.b64encode(file.read()).decode('utf-8')
+    return encoded_string
+
 @api_view(['POST'])
 def process_rosbag(request):
     if request.method == 'POST':
@@ -112,23 +143,32 @@ def process_rosbag(request):
         os.makedirs(output_folder, exist_ok=True)
         os.makedirs(f'{output_folder}/images', exist_ok=True)
 
-        # extract_images_from_rosbag(bag_filename, output_folder)
-        # TODO debug ffmpeg no module found
-        # extract_video(output_folder)
+        extract_images_from_rosbag(bag_filename, output_folder)
+        extract_video(bag_filename, output_folder)
         extract_audio(bag_filename, output_folder)
-        # combine_video_audio(output_folder)
+        combine_video_audio(output_folder)
 
         # Construct paths to the processed files
         video_path = os.path.join(output_folder, 'output.mp4')
         audio_path = os.path.join(output_folder, 'audio.mp3')
-        transcript_path = generate_srt(audio_path, output_folder)
+        output_waveform_path = os.path.join(output_folder, 'output_waveform.png')
 
-        # Return the paths to the processed files
-        # return Response({'message': 'Processing complete'})
+        # plot audio wave graph
+        plot_waveform(audio_path, output_waveform_path)
+
+        #TODO debug the google cloud speech credential issue
+        srt_file_path = generate_srt(audio_path, output_folder)
+
+        # encode the processed files as base64 strings
+        video_data = encode_file_base64(video_path)
+        audio_data = encode_file_base64(audio_path)
+        waveform_image_data = encode_file_base64(output_waveform_path)
+        srt_transcript_data = encode_file_base64(srt_file_path)
+
         return Response({
-            'video_path': video_path,
-            'audio_path': audio_path,
-            'transcript_path': transcript_path,
+            'video_data': video_data,
+            'audio_data': audio_data,
+            'waveform_image_data': waveform_image_data,
             'message': 'Processing complete'
         })
     
