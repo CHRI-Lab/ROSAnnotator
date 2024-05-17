6947af5a2bbfbf11c753f827463aff0e89cc1694
Harry Wang <69313598+Harry-Guanqin-Wang@users.noreply.github.com>
2024-05-17 21:34:43 +1000

    add loading page frontend

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Annotator from './pages/Annotator';
-import LoadableList from './pages/Load';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Annotator from "./pages/Annotator";
+import LoadableList from "./pages/Load";
+import LoadingPage from "./pages/Loading";
+
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Annotator />} />
-        <Route path="/load" element={<LoadableList/>} />
+        <Route path="/load" element={<LoadableList />} />
+        <Route path="/loading" element={<LoadingPage />} />
       </Routes>
     </Router>
   );
