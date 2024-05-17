1c7f292e7fac56a8119f59dcc0ddc5c433650352
Harry Wang <69313598+Harry-Guanqin-Wang@users.noreply.github.com>
2024-05-17 21:34:43 +1000

    add loading page frontend

---
+++ src/frontend/src/pages/Loading/LoadingPage.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { CircularProgress, Box, Typography } from "@mui/material";
+
+const LoadingPage = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <CircularProgress />
+      <Typography
+        sx={{
+          marginTop: 2,
+        }}
+        variant="h6"
+        color="textSecondary"
+      >
+        We are processing your data ...
+      </Typography>
+    </Box>
+  );
+};
+
+export default LoadingPage;
