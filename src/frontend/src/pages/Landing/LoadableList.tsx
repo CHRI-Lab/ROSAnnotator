55fca1d1ebcecca13bea3bfac5183c7ac12317f0
Harry Wang <69313598+Harry-Guanqin-Wang@users.noreply.github.com>
2024-05-18 00:50:21 +1000

    adding mappings for fileNameList to LoadableList

--- src/frontend/src/pages/Landing/LoadableList.tsx
+++ src/frontend/src/pages/Landing/LoadableList.tsx
@@ -1,54 +1,128 @@
-import { List, ListItem, ListItemText, Paper, Typography, Button } from '@mui/material';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Typography,
+  Button,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const fileNameListTest = {
+  rosBagList: ["rosbag1", "rosbag2", "rosbag3"],
+  bookList: ["book1", "book2", "book3"],
+  annotationList: ["annotation1", "annotation2", "annotation3"],
+};
 
 function LoadableList() {
+  const [fileNameList, setFileNameList] = useState(fileNameListTest);
+  const [rosBagFile, setRosBagFile] = useState("");
+  const [bookListFile, setBookListFile] = useState("");
+  const [annotationFile, setAnnotationFile] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // fetch("/api/getFileList")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setFileNameList(data);
+    //   });
+  }, []);
+
+  const handleLoadData = () => {
+    navigate("/main/" + rosBagFile + "+" + bookListFile + "+" + annotationFile);
+  };
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh', padding: '20px', marginTop: '200px' }}>
-    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <Paper style={{ width: '30%' }}>
-          <Typography variant="h6" component="h2" style={{ fontWeight: 'bold', textAlign: 'center' }}>
-            ROSBAG DATA FILE
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100vh",
+        // padding: "20px",
+        // marginTop: "200px",
+      }}
+    >
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <Paper style={{ width: "30%" }}>
+          <Typography
+            variant="h6"
+            component="h2"
+            style={{ fontWeight: "bold", textAlign: "center" }}
+          >
+            ROSBAG DATA FILE *
           </Typography>
           <List component="nav" aria-label="ROSbag data file">
-            <ListItem button>
+            {/* <ListItem button>
               <ListItemText primary="Item 1" />
             </ListItem>
             <ListItem button>
               <ListItemText primary="Item 2" />
-            </ListItem>
+            </ListItem> */}
+            {fileNameList.rosBagList.map((fileName, index) => (
+              <ListItem
+                button
+                key={index}
+                onClick={() => setRosBagFile(fileName)}
+              >
+                <ListItemText primary={fileName} />
+              </ListItem>
+            ))}
           </List>
         </Paper>
-        
-        <Paper style={{ width: '30%' }}>
-          <Typography variant="h6" component="h2" style={{ fontWeight: 'bold', textAlign: 'center' }}>
+
+        <Paper style={{ width: "30%" }}>
+          <Typography
+            variant="h6"
+            component="h2"
+            style={{ fontWeight: "bold", textAlign: "center" }}
+          >
             Predefined Booklist
           </Typography>
           <List component="nav" aria-label="Predefined Booklist">
-            <ListItem button>
-              <ListItemText primary="Book 1" />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary="Book 2" />
-            </ListItem>
+            {fileNameList.bookList.map((fileName, index) => (
+              <ListItem
+                button
+                key={index}
+                onClick={() => setBookListFile(fileName)}
+              >
+                <ListItemText primary={fileName} />
+              </ListItem>
+            ))}
           </List>
         </Paper>
-        
-        <Paper style={{ width: '30%' }}>
-          <Typography variant="h6" component="h2" style={{ fontWeight: 'bold', textAlign: 'center' }}>
+
+        <Paper style={{ width: "30%" }}>
+          <Typography
+            variant="h6"
+            component="h2"
+            style={{ fontWeight: "bold", textAlign: "center" }}
+          >
             Annotation File
           </Typography>
           <List component="nav" aria-label="Annotation File">
-            <ListItem button>
-              <ListItemText primary="File 1" />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary="File 2" />
-            </ListItem>
+            {fileNameList.annotationList.map((fileName, index) => (
+              <ListItem
+                button
+                key={index}
+                onClick={() => setAnnotationFile(fileName)}
+              >
+                <ListItemText primary={fileName} />
+              </ListItem>
+            ))}
           </List>
         </Paper>
       </div>
 
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        <Button variant="contained" color="primary">Load Me</Button>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
+        <Button variant="contained" color="primary" onClick={handleLoadData}>
+          Load Me
+        </Button>
       </div>
     </div>
   );
