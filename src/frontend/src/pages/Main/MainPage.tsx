077dbd815d6a3e6db6556d0fd05f2b5d6b6443b8
Harry Wang <69313598+Harry-Guanqin-Wang@users.noreply.github.com>
2024-05-18 14:47:52 +1000

    add API navigation for file retrieval on Annotator page

--- src/frontend/src/pages/Main/MainPage.tsx
+++ src/frontend/src/pages/Main/MainPage.tsx
@@ -1,35 +1,45 @@
 import React, { useEffect, useState } from "react";
 import LoadingPage from "../Loading";
 import Annotator from "../Annotator";
+import { useParams } from "react-router-dom";
+import { Paper } from "@mui/material";
 // import axios from "axios";
 
 const MainPage = () => {
+  const { rosBagFile, bookListFile } = useParams();
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<unknown>(null);
 
   useEffect(() => {
     // Define the async function to fetch data
     const fetchData = async () => {
-      //   try {
-      //     const response = await axios.get("https://api.example.com/data");
-      //     setData(response.data);
-      //     setLoading(false); // Set loading to false after data is fetched
-      //   } catch (error) {
-      //     setError(error);
-      //     setLoading(false); // Set loading to false even if there's an error
-      //   }
+      try {
+        const response = await fetch(`/api/${rosBagFile}/${bookListFile}`);
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
     };
 
     fetchData();
-  }, []); // Empty dependency array means this effect runs once after the initial render
+  }, []);
 
   if (loading) {
     return <LoadingPage />;
   }
 
   if (error) {
-    // return <div>Error: {error.message}</div>;
+    return (
+      <Paper elevation={3}>
+        <h1>Error</h1>
+        <p>Error loading your files, Please try again.</p>
+        <p>Error: {String(error)}</p>
+      </Paper>
+    );
   }
 
   return (
